65913059412e5f5beb8ecce2ae124dcf3a11fc99
David Otero <domato99.dom@gmail.com>
2021-03-07 03:33:07 +0100

    First commit

---
+++ bot.py
@@ -0,0 +1,31 @@
+# bot.py
+import os
+
+import discord
+import random
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    brooklyn_99_quotes = [
+        'I\'m the human form of the emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+
+    if message.content == '99!':
+        response = random.choice(brooklyn_99_quotes)
+        await message.channel.send(response)
+
+client.run(TOKEN)
